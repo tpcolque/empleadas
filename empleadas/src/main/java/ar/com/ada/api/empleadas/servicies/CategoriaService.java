92d71d8ccf74d1999790357cb036b0cc56399554
tpcolque@hotmail.com <tpcolque@hotmail.com>
2021-06-22 11:47:57 -0300

    Se terminan de completar los primeros webmethods

--- empleadas/src/main/java/ar/com/ada/api/empleadas/servicies/CategoriaService.java
+++ empleadas/src/main/java/ar/com/ada/api/empleadas/servicies/CategoriaService.java
@@ -1,7 +1,23 @@
 package ar.com.ada.api.empleadas.servicies;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ar.com.ada.api.empleadas.entities.Categoria;
+import ar.com.ada.api.empleadas.repos.CategoriaRepository;
+
+@Service
 public class CategoriaService {
 
-    
-    
+    @Autowired
+    CategoriaRepository repo;
+
+    public void crearCategoria(Categoria categoria){
+        repo.save(categoria);
+    }
+    public List<Categoria> traerCategorias(){
+        return repo.findAll();
+    }
 }
